a836edf79e394000950eda8590367ab188922cc6
Brian Thibeault <BrianT321@gmail.com>
2018-12-15 01:17:08 -0500

    Modified Backpack class to include limits.

--- ArtificialLifeGame/src/csc221/alg/model/Backpack.java
+++ ArtificialLifeGame/src/csc221/alg/model/Backpack.java
@@ -1,8 +1,18 @@
 package csc221.alg.model;
 
 
-//TODO: Add limits to adding items
+//TODO: Add limits to adding items                                      // DONE BY Brian
 public class Backpack {
+    private final int WOOD_WEIGHT  = 8;                                 // ADDED BY Brian
+    private final int BERRY_WEIGHT = 2;
+    private final int STEAK_WEIGHT = 4;
+    private final int STONE_WEIGHT = 6;
+    private final int SPEAR_WEIGHT = 5;
+    private final int FLASHLIGHT_WEIGHT = 3;
+
+    private int weight;                                                 // ADDED BY Brian
+    private int space;
+
     private int limit;
     private int woodAmount;
     private int berryAmount;
@@ -12,50 +22,106 @@ public class Backpack {
     private int flashlightAmount;
 
     public Backpack(){
-        this(25,10,0,0,0,1,1);
+        this(25,0,0,0,0,1,1); // REVISED BY Brian
     }
 
-    public Backpack(int limit, int woodAmount, int berryAmount, int steakAmount,int stoneAmount, int spear, int flashlight) {
-
+    public Backpack(int limit, int wood, int berry, int steak,int stone, int spear, int flashlight) { // REVISED BY Brian
         this.limit = limit;
-        this.woodAmount = woodAmount;
-        this.berryAmount = berryAmount;
-        this.steakAmount = steakAmount;
-        this.stoneAmount = stoneAmount;
-        this.spearAmount = spear;
-        this.flashlightAmount = flashlight;
+        woodAmount = wood;
+        berryAmount = berry;
+        steakAmount = steak;
+        stoneAmount = stone;
+        spearAmount = spear;
+        flashlightAmount = flashlight;
+
+        updateWeight();                                                 // ADDED BY Brian
+        if(weight > this.limit) {       // If initial weight exceeds limit then limit is increased to fit all items
+            this.limit = weight;
+        }
     }
 
     public int getWoodAmount() {
         return woodAmount;
     }
 
-    public void addWood(int woodAmount) {
-        this.woodAmount = woodAmount;
+    public boolean addWood(int woodAmount) {                            // REVISED BY Brian
+        if(woodAmount * WOOD_WEIGHT <= space) {
+            this.woodAmount += woodAmount;
+            updateWeight();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+        public void removeWood() {                                      // ADDED BY Brian
+        if(woodAmount > 0) {
+            woodAmount--;
+            updateWeight();
+        }
     }
 
     public int getBerryAmount() {
         return berryAmount;
     }
 
-    public void addBerry(int berryAmount) {
-        this.berryAmount += berryAmount;
+    public boolean addBerry(int berryAmount) {                          // REVISED BY Brian
+        if(berryAmount * BERRY_WEIGHT <= space) {
+            this.berryAmount += berryAmount;
+            updateWeight();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void removeBerry() {                                         // ADDED BY Brian
+        if(berryAmount > 0) {
+            berryAmount--;
+            updateWeight();
+        }
     }
 
     public int getSteakAmount() {
         return steakAmount;
     }
 
-    public void addSteak() {
-        this.steakAmount ++;
+    public boolean addSteak(int steakAmount) {                          // REVISED BY Brian
+        if(steakAmount * STEAK_WEIGHT <= space) {
+            this.steakAmount += steakAmount;
+            updateWeight();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void removeSteak() {                                         // ADDED BY Brian
+        if(steakAmount > 0) {
+            steakAmount--;
+            updateWeight();
+        }
     }
 
     public int getstoneAmount() {
         return stoneAmount;
     }
 
-    public void addStone() {
-        this.stoneAmount ++;
+    public boolean addStone(int stoneAmount) {                          // REVISED BY Brian
+        if(stoneAmount * STONE_WEIGHT <= space) {
+            this.stoneAmount += stoneAmount;
+            updateWeight();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void removeStone() {                                         // ADDED BY Brian
+        if(stoneAmount > 0) {
+            stoneAmount--;
+            updateWeight();
+        }
     }
 
     public int getSpearAmount() {
@@ -69,4 +135,23 @@ public class Backpack {
     public int getLimit() {
         return limit;
     }
+
+    public int getWeight() {                                            // ADDED BY Brian
+        return weight;
+    }
+
+    public int getSpace() {                                             // ADDED BY Brian
+        return space;
+    }
+
+    private void updateWeight() {                                       // ADDED BY Brian
+        weight =  woodAmount  * WOOD_WEIGHT
+                + berryAmount * BERRY_WEIGHT
+                + steakAmount * STEAK_WEIGHT
+                + stoneAmount * STONE_WEIGHT
+                + spearAmount * SPEAR_WEIGHT
+                + flashlightAmount * FLASHLIGHT_WEIGHT;
+        space = limit - weight;
+    }
+
 }
