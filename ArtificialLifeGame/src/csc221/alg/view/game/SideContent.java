ed0ec3b85fa8bd8a1b18b81602ce3f26da1ac1d6
Samuel Fils <smfils1@gmail.com>
2018-12-22 00:16:57 -0500

    Updated View

--- ArtificialLifeGame/src/csc221/alg/view/game/SideContent.java
+++ ArtificialLifeGame/src/csc221/alg/view/game/SideContent.java
@@ -1,6 +1,8 @@
 package csc221.alg.view.game;
 
+import csc221.alg.controller.GameEventHandler;
 import csc221.alg.model.Backpack;
+import csc221.alg.view.menu.ALGButton;
 import javafx.geometry.Insets;
 import javafx.scene.SubScene;
 import javafx.scene.layout.*;
@@ -8,6 +10,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SideContent extends SubScene {
@@ -17,34 +22,64 @@ public class SideContent extends SubScene {
     private Text time;
     private Text rescueTime;
     private Text backpack;
+    private static final int SC_BUTTONS_START_X = 100;
+    private static final int SC_BUTTONS_START_Y = 300;
+    public List<ALGButton> scButtons;
+    private VBox root;
+    private GameEventHandler gameHandler;
 
     public SideContent(){
-        super(new AnchorPane(),WIDTH,HEIGHT);
+        super(new VBox(),WIDTH,HEIGHT);
         prefHeight(WIDTH);
         prefWidth(HEIGHT);
-        AnchorPane root = (AnchorPane) this.getRoot();
+        this.root = (VBox) this.getRoot();
+        this. scButtons = new ArrayList<>();
         root.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY,CornerRadii.EMPTY,Insets.EMPTY)));
-        setLayoutX(600);
-        setLayoutY(0);
+        root.setPadding(new Insets(10,10,10,10));
+        root.setSpacing(10);
         time = new Text("Countdown: 0 s");
         rescueTime = new Text("Rescue Time: ");
         health = new Text("Health: 100 hp");
         backpack = new Text("Inventory: ");
-        time.setLayoutX(10);
-        time.setLayoutY(20);
-        rescueTime.setLayoutX(10);
-        rescueTime.setLayoutY(50);
-        health.setLayoutX(10);
-        health.setLayoutY(80);
-        backpack.setLayoutX(10);
-        backpack.setLayoutY(110);
+
         health.setFont(Font.font("Verdena",23));
         time.setFont(Font.font("Verdena",23));
         rescueTime.setFont(Font.font("Verdena",23));
         backpack.setFont(Font.font("Verdena",23));
-        Line line = new Line(0, 0, 0, HEIGHT);
-        Line line2 = new Line(0, HEIGHT/5, WIDTH, HEIGHT/5);
-        root.getChildren().addAll(line,line2,time,rescueTime, health, backpack);
+       root.getChildren().addAll(time,rescueTime, health, backpack);
+        createButtons();
+    }
+
+
+    public void setGameHandler(GameEventHandler handler) {
+        this.gameHandler = handler;
+    }
+
+    private void createButtons(){
+        createPauseButton();
+        createResumeButton();
+    }
+   public void setBackpack(Backpack bp) {
+        backpack.setText("Inventory: \n" +
+                              "Wood: " + bp.getWoodAmount() +
+                          "\n Berry: " + bp.getBerryAmount() +
+                           "\nSteak: " + bp.getSteakAmount() +
+                           "\nStone: " + bp.getstoneAmount() +
+                           "\nSpear: " + bp.getSpearAmount() +
+                           "\nTorch: " + bp.getFlashlightAmount() +
+                      "\nSpace Left: " + bp.getSpace());
+    }
+
+    private void createPauseButton() {
+        ALGButton pauseButton = new ALGButton("PAUSE");
+        root.getChildren().add(pauseButton);
+        pauseButton.setOnAction(event -> gameHandler.pauseEvent());
+    }
+
+    private void createResumeButton() {
+        ALGButton resumeButton = new ALGButton("RESUME");
+        root.getChildren().add(resumeButton);
+        resumeButton.setOnAction(event -> gameHandler.resumeEvent());
     }
 
     public void setRescueTime(int seconds) {
@@ -56,20 +91,11 @@ public class SideContent extends SubScene {
     public void setTime(int seconds) {
         time.setText("Time: " + seconds + " s");
     }
-    public void setBackpack(Backpack bp) {
-        backpack.setText("Inventory: \n" +
-                              "Wood: " + bp.getWoodAmount() +
-                          "\n Berry: " + bp.getBerryAmount() +
-                           "\nSteak: " + bp.getSteakAmount() +
-                           "\nStone: " + bp.getstoneAmount() +
-                           "\nSpear: " + bp.getSpearAmount() +
-                           "\nTorch: " + bp.getFlashlightAmount() +
-                      "\nSpace Left: " + bp.getSpace());
+
+    public VBox getPane(){
+        return (VBox) this.getRoot();
     }
 
-    public AnchorPane getPane(){
-        return (AnchorPane) this.getRoot();
-    }
 
 
 }
